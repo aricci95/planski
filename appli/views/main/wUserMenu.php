53fed079ee4af8f4888a0e50e461804590099d98
Antoine Ricci <aricci95@gmail.com>
2016-04-01 16:39:24 +0200

    cs

--- appli/views/main/wUserMenu.php
+++ appli/views/main/wUserMenu.php
@@ -1,29 +1,36 @@
-<a href="mailbox" style="position:absolute;right:16%;top: 32px;">
-    <img src="planski/images/icones/message.png" />
-</a>
-<a href="mailbox" style="position:absolute;right:19%;top: 29px;">
-    <img src="planski/images/icones/alert.png" />
-</a>
-<?php if (!empty($this->context->get('new_messages'))) : ?>
-    <div class="counter" style="right:15%;top:13px;"><?php echo $this->context->get('new_messages'); ?></div>
-<?php endif; ?>
-<?php if (!empty($this->context->get('new_notification'))) : ?>
-    <div class="counter" style="right:18%;top:13px;"><?php echo $this->context->get('new_notification'); ?></div>
-<?php endif; ?>
-<?php $photo = empty($this->context->get('user_photo_url')) ? 'unknown.png' : $this->context->get('user_photo_url'); ?>
-<img style="position:absolute;top: 20px;left:86%" class="connectedPhoto" src="planski/photos/profile/<?php echo $photo; ?>" />
-<img class="onglet" style="top: 46px;left: 90%;" src="planski/images/icones/onglet.png" />
-<a style="position:absolute;top:28px;left:89%;" class="dropdown" href="#">
-    <?php echo $this->context->get('user_prenom'); ?><i class="caret" aria-hidden="true"></i>
-</a>
-<ul class="dropdown-menu">
-    <li>
-        <a class="dropdownOption" href="profile/edit">Profil</a>
-    </li>
-    <li>
-        <a class="dropdownOption" href="plan/feed/1">Mes plans</a>
-    </li>
-    <li>
-        <a class="dropdownOption" href="auth/disconnect">Déconnexion</a>
-    </li>
-</ul>
+<div style="float:right;display:inline-block;">
+<div style="float:right;">
+        <img class="onglet" style="" src="planski/images/icones/onglet.png" />
+        <a style="float:right;" class="dropdown" href="#">
+            <?php echo $this->context->get('user_prenom'); ?><i class="caret" aria-hidden="true"></i>
+        </a>
+        <ul class="dropdown-menu">
+            <li>
+                <a class="dropdownOption" href="profile/edit">Profil</a>
+            </li>
+            <li>
+                <a class="dropdownOption" href="plan/feed/1">Mes plans</a>
+            </li>
+            <li>
+                <a class="dropdownOption" href="auth/disconnect">Déconnexion</a>
+            </li>
+        </ul>
+        <?php $photo = empty($this->context->get('user_photo_url')) ? 'unknown.png' : $this->context->get('user_photo_url'); ?>
+        <img style="float:right;" class="connectedPhoto" src="planski/photos/profile/<?php echo $photo; ?>" />
+    </div>
+    <div style="float:right;">
+        <a href="mailbox" style="float:right;">
+            <img src="planski/images/icones/message.png" />
+        </a>
+        <?php if (true || !empty($this->context->get('new_messages'))) : ?>
+            <div class="counter" style="float:right;"><?php echo $this->context->get('new_messages'); ?></div>
+        <?php endif; ?>
+        <a href="mailbox" style="float:right;">
+            <img src="planski/images/icones/alert.png" />
+        </a>
+        <?php if (true || !empty($this->context->get('new_notification'))) : ?>
+            <div class="counter" style=""><?php echo $this->context->get('new_notification'); ?></div>
+        <?php endif; ?>
+    </div>
+
+</div>
